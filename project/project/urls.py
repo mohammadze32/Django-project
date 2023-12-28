518746981ed1e3052d3ba7afb655770c3e305571
Mohammad Zeinalpour <you@example.com>
2023-12-28 22:31:51 +0330

    finishing main page

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from core.views import index
 from core.views import contact , term
@@ -6,5 +8,5 @@ urlpatterns = [
     path('' , index , name = 'index ' ) ,
     path('admin/', admin.site.urls),
     path('contact/' , contact , name= 'contact'),
-    path('term/' , term , name= 'term')
-]
+    path('term/', term , name= 'term')
+] + static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
