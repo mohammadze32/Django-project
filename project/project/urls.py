06baa93d22f5b67994a3614895629f77182605fa
Mohammad Zeinalpour <you@example.com>
2023-12-28 23:37:02 +0330

    detail page sidecolumns

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path , include
 from core.views import index
 from core.views import contact , term
 urlpatterns = [
     path('' , index , name = 'index ' ) ,
+    path('item/' , include('item.urls')),
     path('admin/', admin.site.urls),
     path('contact/' , contact , name= 'contact'),
     path('term/', term , name= 'term')
