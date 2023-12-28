26011e94a6ed59730eaa98b61649f211ea125004
Mohammad Zeinalpour <you@example.com>
2023-12-28 22:16:46 +0330

    finishing rendering : [problem : pylit django-setting.json]

--- project/core/views.py
+++ project/core/views.py
@@ -4,12 +4,11 @@ from item.models import Catagory , item
 def index(request):
     items = item.objects.filter(is_sold=False)[0:2]
     print(items)
-    print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
     catagories = Catagory.objects.all()
 
     return render(request , 'core/index.html' , {
         'catagories' : catagories  ,
-        'itema' : items ,
+        'items' : items ,
     })
 def contact(request):
     return render(request , 'core/contact.html')
