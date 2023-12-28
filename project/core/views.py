ede8448442261e1037f8761dd9eb54ce6e00620d
Mohammad Zeinalpour <you@example.com>
2023-12-28 22:02:49 +0330

    redering items fixing bugs

--- project/core/views.py
+++ project/core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from item.models import Catagory , item
 
-# Create your views here.
-#settig front page
 def index(request):
     items = item.objects.filter(is_sold=False)[0:2]
+    print(items)
+    print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
     catagories = Catagory.objects.all()
 
     return render(request , 'core/index.html' , {
